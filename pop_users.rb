a712bd779b466b90f505f143a5e06ebbc917c02e
Joe <josephbrianchambers@gmail.com>
2013-11-15 11:11:43 -0800

    further prep production db seed

--- pop_users.rb
+++ pop_users.rb
@@ -1,8 +1,7 @@
 
 
 require 'faker'
-ActiveRecord::Base.transaction do
+
   (3..35).to_a.each do |i|
     User.create!(:password => "shammy", :username => Faker::Internet.user_name, :email => Faker::Internet.email)
   end
-end
